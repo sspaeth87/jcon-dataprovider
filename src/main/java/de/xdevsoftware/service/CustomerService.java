641b483c85d6e8a3b82a601a441f8c8d3e39188e
Sebastian Spaeth <p.xplugin@gmail.com>
2023-06-15 11:16:14 +0200

    updates

--- src/main/java/de/xdevsoftware/service/CustomerService.java
+++ src/main/java/de/xdevsoftware/service/CustomerService.java
@@ -9,12 +9,15 @@ import de.xdevsoftware.util.ItemMapper;
 import io.micronaut.http.HttpResponse;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 @Singleton
 public class CustomerService {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerService.class);
 
     @Inject
     CustomerRepository customerRepository;
@@ -23,21 +26,26 @@ public class CustomerService {
     ItemMapper itemMapper;
 
     public HttpResponse<List<CustomerDTO>> findAll() {
+        LOG.info("An attempt is made to load all customers.");
         List<Customer> customers = customerRepository.findAll();
         return HttpResponse.ok(itemMapper.fromList(customers, CustomerDTO.class));
     }
 
     public HttpResponse<CustomerDTO> findById(String id) {
+        LOG.info("An attempt is made to load a customer with the ID: {}.", id);
         Optional<Customer> customer = customerRepository.findById(id);
 
         if(customer.isPresent()) {
+            LOG.info("The customer with ID {} was loaded successfully.", id);
             return HttpResponse.ok(itemMapper.fromItem(customer.get(), CustomerDTO.class));
         }
 
+        LOG.info("No customer with ID {} was found.", id);
         return HttpResponse.notFound();
     }
 
     public HttpResponse<CustomerDTO> save(CustomerCreateDTO customerDTO) {
+        LOG.info("An attempt is made to save a customer with the following data. Firstname: {}, Lastname: {}, E-Mail: {}", customerDTO.getFirstname(), customerDTO.getLastname(), customerDTO.getEmail());
         Customer customer = Customer.builder()
                 .firstname(customerDTO.getFirstname())
                 .lastname(customerDTO.getLastname())
@@ -45,6 +53,7 @@ public class CustomerService {
                 .build();
 
         customerRepository.save(customer);
+        LOG.info("The customer {} {} ({}) was saved successfully.", customerDTO.getFirstname(), customerDTO.getLastname(), customerDTO.getEmail());
         return HttpResponse.created(itemMapper.fromItem(customer, CustomerDTO.class));
     }
 
